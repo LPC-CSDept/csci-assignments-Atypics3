b31f4bad791e2e25c4206090619e3c805beeba14
Edwin <77956997+Atypics3@users.noreply.github.com>
2021-12-15 15:21:16 -0800

    started on q2

--- Final/q2.asm
+++ Final/q2.asm
@@ -6,12 +6,24 @@
 ## Make the interrupt handler (Kernel text program)
 ##
 
-    .data
+    .kdata
+stackPtr1:          .word 10            # acts as a stack pointer
+stackPtr2:          .word 11            # acts as a stack pointer
     .text
     .globl main
 
 main:
+    li  $s0, 113            # $s0 = 'q' (133 in ASCII)
+    mfc0 $a0, $t4           # read from the status reg.
+    ori $a0, 0xff11         # enable all interrupts
+    mtc0 $a0, $t4           # write back to the status reg.
+
+    lui $t0, 0xFFFF         # $t0 = 0xFFFF0000
+    ori $a0, $zero, 2       # enable keyboard interrupt
+    sw  $a0, 0($t0)         # write back to 0xFFFF0000, receiver control data reg.
 
 
+
+exit:
     li  $v0, 10
     syscall
